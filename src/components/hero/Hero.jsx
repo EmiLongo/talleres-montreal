62bbd48ba15ba9fb30a286e241e3471ba4a72695
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2024-05-16 19:17:54 -0300

    se arman variables de css, lo basico de la NavBar y el Hero

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,24 @@
+import './hero.css'
+import hero1 from './img/01-cnc-lathe-machine-guide.png'
+import hero2 from './img/02-machinetool.png'
+import hero3 from './img/03-torno.png'
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <main>
+        <h1>Somos TALLERES MONTREAL. Nos dedicamos a la fabricación y reparación de piezas y equipos para la industria, según planos, muestras o ingeniería propia.</h1>
+      <div className="hero-left">
+        <h2>Fabricación y reparación de piezas</h2>
+        <h3>Hacé tus piezas <i>a partir de muestras o planos.</i></h3>
+      </div>
+      <div className="hero-right">
+        <div className="mask">
+          <div className="img-container">
+            <img src={hero3} alt="" />
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,6 +1,11 @@
+import './navBar.css'
+import logo from './talleres-montreal.svg'
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header>
+      <img src={logo} alt="Logo de Talleres Montreal" height="50" />
+      <div className='text'>TALLERES MONTREAL</div>
+    </header>
   )
 }
 
